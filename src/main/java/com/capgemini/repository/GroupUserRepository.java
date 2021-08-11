dafe5e08dde07c458f088fa9a1b14c7b42d15122
García Sánchez <antonio.garcia-sanchez@capgemini.com>
2021-08-11 17:36:23 +0200

    fix: fixed groupuser respository, mapping class error

--- src/main/java/com/capgemini/repository/GroupUserRepository.java
+++ src/main/java/com/capgemini/repository/GroupUserRepository.java
@@ -3,8 +3,10 @@ package com.capgemini.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.capgemini.model.GroupUserVO;
+
 
 @Repository
-public interface GroupUserRepository extends JpaRepository<GroupUserRepository, Integer> {
+public interface GroupUserRepository extends JpaRepository<GroupUserVO, Integer> {
 
 }
