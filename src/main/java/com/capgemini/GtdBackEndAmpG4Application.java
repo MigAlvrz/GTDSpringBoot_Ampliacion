e96625805eb5800b523d3de56cdc1e02a3fc6853
Miguel Alvarez <miguel-luis.alvarez-lopez@capgemini.com>
2021-08-11 12:01:06 +0200

    Feat: added empty project as first commit

---
+++ src/main/java/com/capgemini/GtdBackEndAmpG4Application.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtdBackEndAmpG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtdBackEndAmpG4Application.class, args);
+	}
+
+}
