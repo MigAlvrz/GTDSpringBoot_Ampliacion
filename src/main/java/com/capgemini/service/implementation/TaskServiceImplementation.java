702d460df9c28cf2d107cd00a5b3857db6025368
García Sánchez <antonio.garcia-sanchez@capgemini.com>
2021-08-12 08:22:53 +0200

    feat: class TaskServiceImplementation created

---
+++ src/main/java/com/capgemini/service/implementation/TaskServiceImplementation.java
@@ -0,0 +1,40 @@
+package com.capgemini.service.implementation;
+
+import java.util.List;
+
+import com.capgemini.model.TaskVO;
+import com.capgemini.service.TaskService;
+
+public class TaskServiceImplementation implements TaskService {
+
+	@Override
+	public TaskVO add(TaskVO task) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TaskVO delete(TaskVO task) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TaskVO deleteById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TaskVO> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TaskVO findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
