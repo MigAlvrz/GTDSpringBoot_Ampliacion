57c4e2e34b870b30015aa9f6189697dcce47ac8a
García Sánchez <antonio.garcia-sanchez@capgemini.com>
2021-08-14 01:25:18 +0200

    feat: implemented and tested with JUnit method listInboxTaskUser

--- src/main/java/com/capgemini/service/implementation/TaskServiceImplementation.java
+++ src/main/java/com/capgemini/service/implementation/TaskServiceImplementation.java
@@ -1,13 +1,17 @@
 package com.capgemini.service.implementation;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.capgemini.model.CategoryVO;
 import com.capgemini.model.TaskVO;
 import com.capgemini.model.UserVO;
+import com.capgemini.repository.CategoryRepository;
 import com.capgemini.repository.TaskRepository;
+import com.capgemini.repository.UserRepository;
 import com.capgemini.service.TaskService;
 
 @Service
@@ -15,7 +19,13 @@ public class TaskServiceImplementation implements TaskService {
 	
 	@Autowired
 	private TaskRepository taskRepo;	
-
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private CategoryRepository catRepo;
+	
 
 	@Override
 	public TaskVO add(TaskVO task) {
@@ -61,6 +71,19 @@ public class TaskServiceImplementation implements TaskService {
 
 	@Override
 	public List<TaskVO> listInboxTask(UserVO user) {
+		if(userRepo.findById(user.getIduser()).isPresent()) {
+			//primero buscamos que exista el usuario, luego buscamos la categoria
+			//sabemos que la categoria INBOX de cada usuario se crea por defecto al
+			//crear un usuario nuevo, por lo que su id=1
+			CategoryVO catInbox=catRepo.findById(1).get();
+			//como hemos de listar las tareas creadas pero no terminadas, la fecha
+			//finished es null
+			LocalDate finished = null;
+			//utilizamos el repo de JPA para buscar la lista de Task con categoria
+			//INBOX por cada user
+			List<TaskVO> listInbox = taskRepo.findByUserTaskAndCategoryTaskAndFinished(user,catInbox,finished);
+			return listInbox;
+		}
 
 		throw new IllegalArgumentException();
 	}
--- src/test/java/com/capgemini/TaskTest.java
+++ src/test/java/com/capgemini/TaskTest.java
@@ -1,17 +1,16 @@
 package com.capgemini;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,6 +19,7 @@ import com.capgemini.model.GroupUserVO;
 import com.capgemini.model.TaskVO;
 import com.capgemini.model.UserStatus;
 import com.capgemini.model.UserVO;
+import com.capgemini.service.CategoryService;
 import com.capgemini.service.TaskService;
 import com.capgemini.service.UserService;
 
@@ -31,6 +31,10 @@ class TaskTest {
 
 	@Autowired
 	private TaskService taskService;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private CategoryService catService;
 	
 	@Test
 	@Order(1)
@@ -128,6 +132,51 @@ class TaskTest {
 		
 		System.out.println("[DONE]\n");
 	}
+	
+	@Test
+	@Order(7)
+	@DisplayName("Listar task con categoria inbox sin terminar")
+	void testListInboxTask() {
+		System.out.println();
+		System.out.println("[TEST 7]");
+		
+		//para poder testear, debemos crear un user,unas categorias para ese user
+		UserVO user1 = new UserVO("user1","user1@capgemini.com","user1",UserStatus.ENABLED,false,new ArrayList<TaskVO>(),new ArrayList<CategoryVO>(),new ArrayList<GroupUserVO>());
+		userService.add(user1);		
+		//categorias para user1
+		CategoryVO cat1user1 = new CategoryVO("INBOX",new ArrayList<TaskVO>(),userService.findById(1));
+		CategoryVO cat2user1 = new CategoryVO("Categoria 2",new ArrayList<TaskVO>(),userService.findById(1));
+		catService.add(cat1user1);
+		catService.add(cat2user1);
+		//creamos unas tareas para user1, el grupo de la tarea lo metemos como null para no sobrecargar el test
+		//creamos 7 tareas pero solo 3 en inbox sin finalizar
+		TaskVO task1user1 = new TaskVO("Tarea 1","lorem ipsum dolor sit amet",LocalDate.now(),null,null,userService.findById(1),catService.findById(1),null);//INBOX sin acabar
+		TaskVO task2user1 = new TaskVO("Tarea 2","lorem ipsum dolor sit amet",LocalDate.now(),null,null,userService.findById(1),catService.findById(1),null);//INBOX sin acabar
+		TaskVO task3user1 = new TaskVO("Tarea 3","lorem ipsum dolor sit amet",LocalDate.now(),null,null,userService.findById(1),catService.findById(1),null);//INBOX sin acabar
+		TaskVO task4user1 = new TaskVO("Tarea 4","lorem ipsum dolor sit amet",LocalDate.now(),null,LocalDate.now(),userService.findById(1),catService.findById(1),null);//INBOX acabada
+		TaskVO task5user1 = new TaskVO("Tarea 5","lorem ipsum dolor sit amet",LocalDate.now(),null,LocalDate.now(),userService.findById(1),catService.findById(1),null);//INBOX acabada
+		TaskVO task6user1 = new TaskVO("Tarea 6","lorem ipsum dolor sit amet",LocalDate.now(),null,null,userService.findById(1),catService.findById(2),null);//Categoria 2 sin acabar
+		TaskVO task7user1 = new TaskVO("Tarea 7","lorem ipsum dolor sit amet",LocalDate.now(),null,null,userService.findById(1),catService.findById(2),null);//Categoria 2 sin acabar
+		taskService.add(task1user1);
+		taskService.add(task2user1);
+		taskService.add(task3user1);
+		taskService.add(task4user1);
+		taskService.add(task5user1);
+		taskService.add(task6user1);
+		taskService.add(task7user1);
+		
+		List<TaskVO> listTaskInboxUser1 = taskService.listInboxTask(user1);
+		for (TaskVO t : listTaskInboxUser1) {
+			System.out.println("Soy la "+t.getTitle()+" de la categoria "+t.getCategoryTask().getName()+" del "+t.getUserTask().getLogin());
+		}
+		
+		//deberíamos encontrar 3 tareas en INBOX sin finalizar para el user1
+		assertEquals(3, taskService.listInboxTask(user1).size());
+
+		
+		System.out.println("[DONE]\n");
+	}
+	
 	
 	
 }
