aa0e0f8640b3bda999630c2f76ee733faafff903
Miguel Alvarez <miguel-luis.alvarez-lopez@capgemini.com>
2021-08-16 19:27:27 +0200

    feature: added javadoc for groupController

--- src/main/java/com/capgemini/controller/GroupController.java
+++ src/main/java/com/capgemini/controller/GroupController.java
@@ -21,6 +21,12 @@ public class GroupController {
 	@Autowired
 	GroupService grService;
 	
+	/**
+	 * Creates a GroupVO, then adds it to the DB
+	 * @param body
+	 * @return
+	 */
+	
 	@PostMapping("/add")
 	public ResponseEntity<?> add(@RequestBody String body){
 		GroupVO newGroup = null;
@@ -34,11 +40,31 @@ public class GroupController {
 		return checkNull(gr);
 	}
 	
+	/**
+	 * Deletes an existing groupVO from the DB
+	 * @param body
+	 * @return
+	 */
+	
 	@DeleteMapping("/delete")
-	public ResponseEntity<?> delete(@RequestBody GroupVO group){
+	public ResponseEntity<?> delete(@RequestBody String body){
+		GroupVO group = null;
+		try {
+			group = new ObjectMapper().readValue(body,GroupVO.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 		GroupVO gr = grService.delete(group);
 		return checkNull(gr);
 	}
+	
+	/**
+	 * Finds a groupVO by it's ID, then deletes it
+	 * @param id
+	 * @return
+	 */
 	
 	@DeleteMapping("/delete/{idcategory}")
 	public ResponseEntity<?> deleteById(@RequestBody int id){
@@ -46,16 +72,34 @@ public class GroupController {
 		return checkNull(gr);
 	}
 	
+	/**
+	 * returns all the existing categories from the DB
+	 * @return
+	 */
+	
 	@GetMapping("/listAll")
 	public ResponseEntity<?> findAll(){
 		return new ResponseEntity<>(grService.listAll(),HttpStatus.OK);
 	}
+	
+	/**
+	 * Finds a category by it's ID
+	 * @param id
+	 * @return
+	 */
 	
 	@GetMapping("/find/{idcategory}")
 	public ResponseEntity<?> findById(@RequestBody int id){
 		GroupVO gr = grService.findById(id);
 		return checkNull(gr);
 	}
+	
+	/**
+	 * Checks if a GroupVO is correct, then returns an Ok httpStatus
+	 * otherwise, returns a Bad_Request
+	 * @param cat
+	 * @return
+	 */
 	
 	private ResponseEntity<?> checkNull(GroupVO gr) {
 		if(gr!=null)
