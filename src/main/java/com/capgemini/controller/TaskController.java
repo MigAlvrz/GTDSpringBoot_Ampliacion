4a5258cc3a0701447739247436c65cc5098228cd
Miguel Alvarez <miguel-luis.alvarez-lopez@capgemini.com>
2021-08-16 19:41:23 +0200

    feature: added Javadoc for TaskController

--- src/main/java/com/capgemini/controller/TaskController.java
+++ src/main/java/com/capgemini/controller/TaskController.java
@@ -30,6 +30,12 @@ public class TaskController {
 	@Autowired
 	private CategoryService catService;
 
+	/**
+	 * Creates a new TaskVO, then adds it to the DB
+	 * @param body
+	 * @return
+	 */
+	
 	@PostMapping("/add")
 	public ResponseEntity<?> add(@RequestBody String body) {
 		TaskVO newTask = null;
@@ -42,30 +48,66 @@ public class TaskController {
 		taskService.add(newTask);
 		return checkNull(newTask);
 	}
+	
+	/**
+	 * Deletes a TaskVo from the DB
+	 * @param body
+	 * @return
+	 */
 
 	@DeleteMapping("/delete")
-	public ResponseEntity<?> delete(@RequestBody TaskVO task) {
-		TaskVO exTask = taskService.delete(task);
+	public ResponseEntity<?> delete(@RequestBody String body) {
+		TaskVO exTask = null;
+		try {
+			exTask = new ObjectMapper().readValue(body, TaskVO.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		taskService.delete(exTask);
 		return checkNull(exTask);
 	}
 
+	/**
+	 * Finds a TaskVo by it's Id, then deletes it from the database
+	 * @param taskId
+	 * @return
+	 */
+	
 	@DeleteMapping("/delete/{taskId}")
 	public ResponseEntity<?> deleteById(@PathVariable int taskId) {
 		TaskVO task = taskService.deleteById(taskId);
 		return checkNull(task);
 	}
+	
+	/**
+	 * Returns all the existing TaskVos from the DB
+	 * @return
+	 */
 
 	@GetMapping("/listAll")
 	public ResponseEntity<?> findAll() {
 		return new ResponseEntity<>(taskService.listAll(), HttpStatus.OK);
 	}
 
-	@GetMapping("/find/{UserId}")
-	public ResponseEntity<?> findById(@PathVariable int userId) {
-		TaskVO task = taskService.findById(userId);
+	/**
+	 * Finds a TaskVO by it's ID
+	 * @param TaskId
+	 * @return
+	 */
+	
+	@GetMapping("/find/{TaskId}")
+	public ResponseEntity<?> findById(@PathVariable int TaskId) {
+		TaskVO task = taskService.findById(TaskId);
 		return checkNull(task);
 	}
 
+	/**
+	 * Returns all the existing TaskVOs from an UserVO
+	 * @param userId
+	 * @return
+	 */
+	
 	@GetMapping("/listInbox/{userId}")
 	public ResponseEntity<?> findInbox(@PathVariable int userId) {
 		if (userService.findById(userId) != null) {
@@ -74,6 +116,12 @@ public class TaskController {
 		}
 		return new ResponseEntity<>(userId, HttpStatus.BAD_REQUEST);
 	}
+	
+	/**
+	 * Returns all the TaskVOs planned for today
+	 * @param userId
+	 * @return
+	 */
 	
 	@GetMapping("/listToday/{userId}")
 	public ResponseEntity<?> findToday(@PathVariable int userId) {
@@ -84,6 +132,12 @@ public class TaskController {
 		return new ResponseEntity<>(userId, HttpStatus.BAD_REQUEST);
 	}
 	
+	/**
+	 * Returns all the TaskVOs planned for this week
+	 * @param userId
+	 * @return
+	 */
+	
 	@GetMapping("/listWeek/{userId}")
 	public ResponseEntity<?> findWeek(@PathVariable int userId) {
 		if (userService.findById(userId) != null) {
@@ -92,6 +146,13 @@ public class TaskController {
 		}
 		return new ResponseEntity<>(userId, HttpStatus.BAD_REQUEST);
 	}
+	
+	/**
+	 * Returns all the TaskVOs from an specific CategoryVO
+	 * @param userId
+	 * @param categoryId
+	 * @return
+	 */
 	
 	@GetMapping("/listCategory/{userId}/{categoryId}")
 	public ResponseEntity<?> findCategory(@PathVariable int userId, @PathVariable int categoryId) {
@@ -103,6 +164,13 @@ public class TaskController {
 		return new ResponseEntity<>(userId, HttpStatus.BAD_REQUEST);
 	}
 
+	/**
+	 * Checks if a TaskVO is correct, then returns an Ok httpStatus
+	 * otherwise, returns a Bad_Request
+	 * @param cat
+	 * @return
+	 */
+	
 	private ResponseEntity<?> checkNull(TaskVO task) {
 		if (task != null)
 			return new ResponseEntity<>(task, HttpStatus.OK);
