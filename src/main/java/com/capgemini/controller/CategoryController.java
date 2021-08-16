884083a777e3049659a4652db7e49cc61616037d
Miguel Alvarez <miguel-luis.alvarez-lopez@capgemini.com>
2021-08-16 19:21:36 +0200

    Feature: added javadoc to categoryController

--- src/main/java/com/capgemini/controller/CategoryController.java
+++ src/main/java/com/capgemini/controller/CategoryController.java
@@ -22,6 +22,12 @@ public class CategoryController {
 	@Autowired
 	CategoryService catService;
 	
+	/**
+	 * Creates a new CategoryVO and adds it to the DB
+	 * @param body
+	 * @return
+	 */
+	
 	@PostMapping("/add")
 	public ResponseEntity<?> add(@RequestBody String body){
 		CategoryVO newCat = null;
@@ -35,11 +41,31 @@ public class CategoryController {
 		return checkNull(cat);
 	}
 	
+	/**
+	 * Deletes a CategoryVO from the DB
+	 * @param category
+	 * @return
+	 */
+	
 	@DeleteMapping("/delete")
-	public ResponseEntity<?> delete(@RequestBody CategoryVO category){
-		CategoryVO cat = catService.delete(category);
-		return checkNull(cat);
+	public ResponseEntity<?> delete(@RequestBody String body){
+		CategoryVO CategoryToBeDeleted = null;
+		try {
+			CategoryToBeDeleted = new ObjectMapper().readValue(body, CategoryVO.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		CategoryVO cat = catService.delete(CategoryToBeDeleted);
+		return checkNull(CategoryToBeDeleted);
 	}
+	
+	/**
+	 * Finds a CategoryVO by it's ID and deletes it
+	 * @param idcategory
+	 * @return
+	 */
 	
 	@DeleteMapping("/delete/{idcategory}")
 	public ResponseEntity<?> deleteById(@PathVariable int idcategory){
@@ -47,16 +73,34 @@ public class CategoryController {
 		return checkNull(cat);
 	}
 	
+	/**
+	 * returns all the categoryVOs available in the DB
+	 * @return
+	 */
+	
 	@GetMapping("/listAll")
 	public ResponseEntity<?> findAll(){
 		return new ResponseEntity<>(catService.listAll(),HttpStatus.OK);
 	}
+	
+	/**
+	 * Finds a categoryVO by id
+	 * @param idcategory
+	 * @return
+	 */
 	
 	@GetMapping("/find/{idcategory}")
 	public ResponseEntity<?> findById(@PathVariable int idcategory){
 		CategoryVO cat = catService.findById(idcategory);
 		return checkNull(cat);
 	}
+	
+	/**
+	 * Checks if a categoryVO is correct, then returns an Ok httpStatus
+	 * otherwise, returns a Bad_Request
+	 * @param cat
+	 * @return
+	 */
 	
 	private ResponseEntity<?> checkNull(CategoryVO cat) {
 		if(cat!=null)
